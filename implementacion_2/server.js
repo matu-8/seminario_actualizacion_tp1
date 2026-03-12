22f624accff943980c5d491b5401ffc1ece7fce3
Matias Agüero <agueromatias214@gmail.com>
2026-03-12 20:50:20 -0300

    Servidor creado, junto con la pagina con el ejercicio

---
+++ implementacion_2/server.js
@@ -0,0 +1,18 @@
+import {createServer} from 'node:http'
+import { readFile } from 'node:fs/promises';
+
+const PORT = 8080;
+const server = createServer(async(req, res)=>{
+try {
+    const file = await readFile('./index.html');
+    res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
+    res.end(file)
+    
+} catch (error) {
+    res.writeHead(404, {"content-type":"text/plain"});
+    res.end("No se ha encontrado la pagina, lo sentimos")
+    }
+});
+server.listen(PORT, ()=>{
+    console.log(`>>> seridor corriendo en http://localhost:${PORT}`)
+})
