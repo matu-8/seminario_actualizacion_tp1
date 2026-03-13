5259939433b3345adc6e96a53adc1101ec2436f6
Matias Agüero <agueromatias214@gmail.com>
2026-03-13 02:08:29 -0300

    servidor con python utilizando librería nativa, creado

---
+++ implementacion_3/server.py
@@ -0,0 +1,23 @@
+from wsgiref.simple_server import make_server
+
+# En python, environ y start_response, de manera basica, serian como reques y response en nodeJS
+def application(environ, start_response):
+    try:
+        with open('./index.html', 'rb') as f:
+            contenido = f.read()
+            headers = [
+                ("Content-Type","text/html; charset=utf-8"),
+                ('Content-Length', str(len(contenido)))
+    ]
+        start_response('200 OK', headers)
+        return[contenido]
+
+    except FileNotFoundError:
+        status = '404 Not Found'
+        headers = [('Content-Type', 'text/plain')]
+        start_response(status, headers)
+        return[b"Archivo no encontrado"]
+
+server = make_server('localhost', 8000, application)
+print('>>> Servidor corriendo en http://localhost:8000')
+server.serve_forever()
